0815b396b21e9c4955fc6b95140d208526685b37
Charan <121006199+Charan2999@users.noreply.github.com>
2022-12-25 23:43:10 +0530

    Update counter.js

--- stellar/assets/js/counter.js
+++ stellar/assets/js/counter.js
@@ -6,7 +6,13 @@ function updateCounter() {
          * if count is null then set count to 1
         * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
         */
+        if (count == null){
+                count = 1;
+        }
+        else{
+                count += 1;
+        }
         localStorage.setItem('visiterCount', count);
-        counter.outerHTML = `Vistor Count: ${localStorage.getItem('visiterCount')}`;
+        counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterCount')}`;
     }
 updateCounter();
