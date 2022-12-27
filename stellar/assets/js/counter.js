d1055f1482d2b0699defd27f83ee6e6fdafdeefb
Charan <121006199+Charan2999@users.noreply.github.com>
2022-12-28 00:00:46 +0530

    Update counter.js

--- stellar/assets/js/counter.js
+++ stellar/assets/js/counter.js
@@ -1,7 +1,10 @@
 function updateCounter() {
     const counter = document.getElementById('counter');
+    const list = document.getElementById('list');
+    
     //increment the counter everytime user visits the page by storing in local storage
     let count = localStorage.getItem('visiterCount');
+    let vList = localStorage.getItem('visiterLocation');
     /**
      * if count is null then set count to 1
     * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
@@ -12,7 +15,10 @@ function updateCounter() {
     else{
             count  =parseInt(count)+ 1;
     }
+    vList = vList + list
     localStorage.setItem('visiterCount', count);
-    counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterCount')}`;
+    localStorage.setItem('visiterLocation', count);
+    counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterCount')
+    list.innerHTML = `Vistor location: ${localStorage.getItem('visiterLocation')}`;
 }
 updateCounter();
